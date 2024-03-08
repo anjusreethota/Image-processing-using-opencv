f7476e110c44ca60580d602ebd9427ef6695f25f
Anjusree Thota <103201221+anjusreethota@users.noreply.github.com>
2024-03-08 12:03:02 +0530

    Create subimage.py

---
+++ subimage.py
@@ -0,0 +1,15 @@
+import cv2
+
+fname = input("Enter the path of the image - 1: ")
+img_arr = cv2.imread(fname)
+cv2.imshow("Image - 1", img_arr)
+fname2 = input("Enter the path of the image - 2: ")
+img_arr_2 = cv2.imread(fname2)
+img_arr_2 = cv2.resize(img_arr_2, img_arr.shape[:2][::-1])
+cv2.imshow("Image - 2 after resizing", img_arr_2)
+sub_arr_1 = cv2.subtract(img_arr, img_arr_2)
+cv2.imshow("Image - 1 - Image - 2", sub_arr_1)
+sub_arr_2 = cv2.subtract(img_arr_2, img_arr)
+cv2.imshow("Image - 2 - Image - 1", sub_arr_2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
